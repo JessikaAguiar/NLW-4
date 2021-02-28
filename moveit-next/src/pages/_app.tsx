b41b08690d716b17863ea63552c6775755c9977e
Jéssika Aguiar <jessika.developer@gmail.com>
2021-02-28 00:00:49 -0400

    Tratando persistencia de dados e criando modal  #missioncomplete

--- moveit-next/src/pages/_app.tsx
+++ moveit-next/src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faPlay, faCheckCircle, faTimes } from '@fortawesome/free-solid-svg-icons'
-import { ChallengesProvider } from '../contexts/ChallengesContext';
 
 import '../styles/global.css';
 
@@ -9,9 +8,9 @@ library.add(fab, faPlay, faCheckCircle, faTimes)
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChallengesProvider>
-      <Component {...pageProps} />
-    </ChallengesProvider>
+
+    <Component {...pageProps} />
+
   )
 }
 
--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { GetServerSideProps } from 'next';
 
 import { CompletedChanllenges } from "../components/CompletedChanllenges";
 import { CountDown } from "../components/CountDown";
@@ -6,12 +7,23 @@ import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import { ChanllengeBox } from "../components/ChanllengeBox";
 import { CountDownProvider } from '../contexts/CountDownContext';
+import { ChallengesProvider } from '../contexts/ChallengesContext';
 
 import styles from "../styles/pages/Home.module.css";
 
-export default function Home() {
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+export default function Home(props: HomeProps) {
   return (
-    <div className={styles.container}>
+    <ChallengesProvider 
+    level={props.level}
+    currentExperience={props.currentExperience}
+    challengesCompleted={props.challengesCompleted}>
+    '<div className={styles.container}>
       <Head>
         <title>Inicio | Moveit</title>
       </Head>
@@ -30,6 +42,19 @@ export default function Home() {
           </div>
         </section>
       </CountDownProvider>
-    </div>
+    </div>'
+    </ChallengesProvider>
   );
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted)
+    }
+  }
 }
